980b61ec91bfdbd83be176ba81d8b555706aa29d
Arsrillon <papapapa1990papa@gmail.com>
2026-02-11 00:39:33 +0200

    refactor: switch to ClientCommandManager for server compatibility

--- src/main/java/net/marblock/fakefps/FakeFPS.java
+++ src/main/java/net/marblock/fakefps/FakeFPS.java
@@ -1,32 +1,31 @@
 package net.marblock.fakefps;
 
-import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
-import static net.minecraft.server.command.CommandManager.*;
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
 import com.mojang.brigadier.arguments.IntegerArgumentType;
 import net.minecraft.text.Text;
 
-public class FakeFPS implements ModInitializer {
+public class FakeFPS implements ClientModInitializer {
     public static final String MOD_ID = "fakefps";
-
     public static int fakeFpsAmount = 1000;
 
     @Override
-    public void onInitialize() {
-        CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
-            dispatcher.register(literal("fakefps")
-                    .then(argument("amount", IntegerArgumentType.integer())
+    public void onInitializeClient() {
+        ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> {
+            dispatcher.register(ClientCommandManager.literal("fakefps")
+                    .then(ClientCommandManager.argument("amount", IntegerArgumentType.integer())
                             .executes(context -> {
                                 int value = IntegerArgumentType.getInteger(context, "amount");
                                 fakeFpsAmount = value;
-                                context.getSource().sendFeedback(() -> Text.literal("Fake FPS set to: " + value), false);
+                                context.getSource().sendFeedback(Text.literal("§aFake FPS set to: " + value));
                                 return 1;
                             })
                     )
-                    .then(literal("off")
+                    .then(ClientCommandManager.literal("off")
                             .executes(context -> {
                                 fakeFpsAmount = 0;
-                                context.getSource().sendFeedback(() -> Text.literal("Fake FPS disabled."), false);
+                                context.getSource().sendFeedback(Text.literal("§cFake FPS disabled."));
                                 return 1;
                             })
                     )
